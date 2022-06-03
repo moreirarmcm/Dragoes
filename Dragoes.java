636bd3a19830a35509cf7158acf1190246ffd0a4
Renan Moreira <105755795+moreirarmcm@users.noreply.github.com>
2022-06-03 12:22:27 -0300

    Update Dragoes.java

--- Dragoes.java
+++ Dragoes.java
@@ -6,23 +6,34 @@ import java.util.Random;
 import java.util.Scanner;
 
 /**
- * Codigo "hobbie" 
- * Cria "dragoes", com caracteristicas especificas, e os reproduz.
+ * Código hobbie "CRIA DRAGÕES"
  * 
- * Verde - V-80%	A-15%	A-5%	V-0%	P-0%
- * Azul - V-30%	A-55%	A-15%	V-0%	P-0%
- * Amarelo - V-10%	A-25%	A-60%	V-10%	P-0%
- * Vermelho - V-5%	A-20%	A-45%	V-25%	P-5%
- * Preto - V-%	A-9%	A-40%	V-30%	P-20% B - 1%
+ *  
+ * Há 5 raças de dragões: verdes, azuis, amarelos, vermelhos e pretos. Verde é a raça mais comum (inicial), preto é a mais rara.
+ * Cada raça possui uma probabilidade de gerar filhotes de outras raças, seguindo a seguinte tabela (valores-padrão de raça):
+ *
+ * 					Raça1	Raça2	Raça3	Raça4	Raça5
+ * 	Prob. verde: 	80%		30%		10%		5%		1%
+ * 	Prob. azul: 	15%		55%		25%		20%		9%
+ * 	Prob. amarelo:	5%		15%		60%		45%		20%
+ * 	Prob. vermelho:	0%		0%		10%		25%		40%
+ * 	Prob. preto:	0%		5%		0%		0%		30%
  * 
+ *   
+ *  Para cada novo dragão criado têm-se a seguinte regra:
+ *  	- caso a raça do dragão "filhote" seja superior às raças dos pais, o filhote recebe os valores-padrão de probabilidade.
+ *  	- caso a raça do filhote seja igual ou menor que as raças dos pais, o filhote receberá os valores dados pela média dos 
+ *  	valores dos pais, convertido para taxa percentual (em double).
+ *  		ex: o dragão Azul, filho dos dragões Amarela e Vermelho, receberá os valores: Verde-7,32%	Azul-21,95%		Amarelo-51,22%		Vermelho-17,07%		Preto-2,44%
+ *  			(a soma das taxas percentuais deve ser sempre = 100%).
+ *  
  * @author Renan
  *
  */
 
 public class Dragoes {
-	
-	int raca; // raça = verde, azul, amarelo, vermelho e preto. raca recebe 1-5; 
-	int sexo; // 0 para fêmea, 1 para macho.
+	int raca; 
+	int sexo; 
 	double probVerde;
 	double probAzul;
 	double probAmarelo;
@@ -31,22 +42,26 @@ public class Dragoes {
 	double probBranco;
 	
 	public static void main(String[] args) {
-		//Scanner sc = new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		DragoesMethod dg = new DragoesMethod();
-		List <Dragoes> dragao2;
-		dg.População(10000);
+		List <Dragoes> dragao = new ArrayList<>();
 		dg.AdaoEva();
-		dragao2 = dg.Dragao2Todo();
-		for (int i = 0; i< dragao2.size(); i++) {
-			System.out.println((i+1) + "° ----" + dragao2.get(i).raca + "----" + dragao2.get(i).sexo);
-
+		//dragao.add((Dragoes) dg.dragao);
+		int x = 1;
+		for (int escolha = 0; escolha < 1; escolha=escolha) {
+			dg.NovoDragao();
+			dragao.clear();
+			dragao.addAll(dg.dragao);
+			escolha= sc.nextInt();
+			if (escolha == 0) {
+				System.out.println(x + "ra GERAÇÃO" );
+				x++;
+				for (int i = 0; i < dragao.size(); i++) {
+					System.out.println ((i + 1) + "° dragão | " + "Tipo: " + dragao.get(i).raca +	" | Sexo: " + dragao.get(i).sexo + 
+							" | probVerde: " + dragao.get(i).probVerde + " | probAzul: " + dragao.get(i).probAzul + " | probAmarelo: " + dragao.get(i).probAmarelo + 
+							" | probVermelho: " + dragao.get(i).probVermelho +" | probPreto " + dragao.get(i).probPreto);
+				}
+			}
 		}
-
-		dg.CriaDragao();
-		//Dragoes [] dragao = dg.DragaoTodo();
-		 dragao2 = dg.Dragao2Todo();
-		
-	//	System.out.println(dragao2.get(0).sexo);
-		
 	}
 }
